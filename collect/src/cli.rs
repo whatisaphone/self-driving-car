8f1b3e5e7c80978c01652e0696fd7dc98ab8fcb0
John Simon <john@whatisaph.one>
2018-08-31 21:10:20 -0400

    Use external rlbot

--- collect/src/cli.rs
+++ collect/src/cli.rs
@@ -1,17 +1,14 @@
 use bakkesmod::BakkesMod;
 use collector::Collector;
 use rlbot;
-use rlbot::RLBotError;
 use std::error::Error;
 use std::fs::File;
 use std::thread::sleep;
 use std::time::Duration;
 
 pub fn main() -> Result<(), Box<Error>> {
-    let rlbot = rlbot::init().map_err(|_| RLBotError)?;
-    rlbot
-        .start_match(rlbot::MatchSettings::simple_1v1())
-        .map_err(|_| RLBotError)?;
+    let rlbot = rlbot::init()?;
+    rlbot.start_match(rlbot::MatchSettings::simple_1v1())?;
 
     let mut packets = rlbot.packeteer();
 
@@ -19,11 +16,9 @@ pub fn main() -> Result<(), Box<Error>> {
     while !packets.next()?.GameInfo.RoundActive {}
 
     // Zero out our input, just to be safe
-    rlbot
-        .update_player_input(Default::default(), 0)
-        .map_err(|_| RLBotError)?;
+    rlbot.update_player_input(Default::default(), 0)?;
 
-    let bakkesmod = BakkesMod::connect().map_err(|_| RLBotError)?;
+    let bakkesmod = BakkesMod::connect()?;
     let commands = [
         "ball location 2000 0 0",
         "ball velocity 0 0 0",
@@ -50,32 +45,24 @@ pub fn main() -> Result<(), Box<Error>> {
         // The action we want to record:
         if packet.GameInfo.TimeSeconds < start + 1.0 {
             let input = Default::default();
-            rlbot
-                .update_player_input(input, 0)
-                .map_err(|_| RLBotError)?;
+            rlbot.update_player_input(input, 0)?;
         } else if packet.GameInfo.TimeSeconds < start + 3.0 {
             let input = rlbot::PlayerInput {
                 Throttle: 1.0,
                 Boost: true,
                 ..Default::default()
             };
-            rlbot
-                .update_player_input(input, 0)
-                .map_err(|_| RLBotError)?;
+            rlbot.update_player_input(input, 0)?;
         } else if packet.GameInfo.TimeSeconds < start + 8.0 {
             let input = Default::default();
-            rlbot
-                .update_player_input(input, 0)
-                .map_err(|_| RLBotError)?;
+            rlbot.update_player_input(input, 0)?;
         } else {
             break;
         }
     }
 
     // Stop the car before exiting (just like IRL!)
-    rlbot
-        .update_player_input(Default::default(), 0)
-        .map_err(|_| RLBotError)?;
+    rlbot.update_player_input(Default::default(), 0)?;
 
     Ok(())
 }
