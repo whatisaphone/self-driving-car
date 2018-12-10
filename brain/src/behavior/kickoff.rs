635819afa15e103d4fd162422bfbc4e1b5f38b6f
John Simon <john@whatisaph.one>
2018-12-09 23:48:27 -0500

    Add a kickoff test

--- brain/src/behavior/kickoff.rs
+++ brain/src/behavior/kickoff.rs
@@ -30,3 +30,24 @@ impl Behavior for Kickoff {
         ))
     }
 }
+
+#[cfg(test)]
+mod integration_tests {
+    use crate::{integration_tests::helpers::TestRunner, strategy::Runner2};
+    use brain_test_data::recordings;
+    use common::prelude::*;
+
+    #[test]
+    fn kickoff_center() {
+        let test = TestRunner::new()
+            .one_v_one(&*recordings::KICKOFF_CENTER, 107.0)
+            .behavior(Runner2::soccar())
+            .run_for_millis(3000);
+
+        let packet = test.sniff_packet();
+        let ball_loc = packet.GameBall.Physics.locp();
+        let ball_vel = packet.GameBall.Physics.vel();
+        let future = ball_loc + ball_vel * 3.0;
+        assert!(future.y >= -5000.0);
+    }
+}
