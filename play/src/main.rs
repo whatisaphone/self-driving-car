e00af32dce230a45f75d2a44589d249be93872c6
John Simon <john@whatisaph.one>
2018-12-13 12:26:19 -0500

    Print more useful panic messages on startup

--- play/src/main.rs
+++ play/src/main.rs
@@ -14,7 +14,7 @@ use brain::{Brain, EEG};
 use chrono::Local;
 use collect::{get_packet_and_inject_rigid_body_tick, Collector};
 use common::ext::ExtendRLBot;
-use std::{env, fs, panic, path::PathBuf, thread::sleep, time::Duration};
+use std::{env, error::Error, fs, panic, path::PathBuf, thread::sleep, time::Duration};
 
 mod logging;
 
@@ -24,18 +24,15 @@ fn main() {
         .format(logging::format)
         .init();
 
-    let rlbot = rlbot::init().unwrap();
+    let rlbot = rlbot::init().expect("Could not initialize RLBot");
     let rlbot: &rlbot::RLBot = Box::leak(Box::new(rlbot));
 
-    let mut args = env::args();
-    args.next().unwrap(); // Program name
-    let use_framework = args.next().as_ref().map(String::as_str) == Some("--player-index");
-    let player_index = if use_framework {
-        args.next().unwrap().parse().unwrap()
-    } else {
-        start_match(&rlbot);
-        0
-    };
+    let (use_framework, should_start_match, player_index) =
+        parse_args().expect("Error parsing command-line arguments");
+
+    if should_start_match {
+        start_match(&rlbot).expect("Error starting match");
+    }
 
     if !use_framework {
         // In dev mode, halt on panics so they can't be ignored.
@@ -46,7 +43,22 @@ fn main() {
     }
 }
 
-fn start_match(rlbot: &rlbot::RLBot) {
+fn parse_args() -> Result<(bool, bool, i32), ()> {
+    let mut args = env::args();
+    args.next().ok_or(())?; // Program name
+    let use_framework = args.next().as_ref().map(String::as_str) == Some("--player-index");
+    let (should_start_match, player_index);
+    if use_framework {
+        should_start_match = false;
+        player_index = args.next().ok_or(())?.parse().map_err(|_| ())?
+    } else {
+        should_start_match = true;
+        player_index = 0;
+    }
+    Ok((use_framework, should_start_match, player_index))
+}
+
+fn start_match(rlbot: &rlbot::RLBot) -> Result<(), Box<Error>> {
     let match_settings = rlbot::ffi::MatchSettings {
         MutatorSettings: rlbot::ffi::MutatorSettings {
             MatchLength: rlbot::ffi::MatchLength::Unlimited,
@@ -54,8 +66,8 @@ fn start_match(rlbot: &rlbot::RLBot) {
         },
         ..rlbot::ffi::MatchSettings::simple_1v1("Formula None", "All-Star")
     };
-    rlbot.start_match(match_settings).unwrap();
-    rlbot.wait_for_match_start().unwrap();
+    rlbot.start_match(match_settings)?;
+    rlbot.wait_for_match_start()
 }
 
 /// Keep running the given function until it doesn't panic.
@@ -108,14 +120,14 @@ fn create_collector() -> Collector {
 
     let now = Local::now().format("%Y-%m-%d_%H.%M.%S").to_string();
     let filename = format!("{}/play-{}.csv", directory, now);
-    let file = fs::File::create(&filename).unwrap();
+    let file = fs::File::create(&filename).expect("Error creating game log file");
 
     // Link a consistently-named file, for convenience.
     let link = PathBuf::from(format!("{}/play.csv", directory));
     if link.exists() {
-        fs::remove_file(&link).unwrap();
+        fs::remove_file(&link).expect("Error deleting old game log hard link");
     }
-    fs::hard_link(filename, &link).unwrap();
+    fs::hard_link(filename, &link).expect("Error creating game log hard link");
 
     Collector::new(file)
 }
