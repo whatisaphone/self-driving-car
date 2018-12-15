84a5b677730d045053cfc20a31c30889ac5beb08
John Simon <john@whatisaph.one>
2018-12-15 00:10:20 -0500

    Bail on this degenerate state

--- brain/src/routing/plan/pathing.rs
+++ brain/src/routing/plan/pathing.rs
@@ -40,6 +40,10 @@ pub fn avoid_goal_wall_waypoint(start: &CarState, target_loc: Point2<f32>) -> Op
     if (brink - start.loc.y).signum() == (brink - target_loc.y).signum() {
         return None;
     }
+    // This is a degenerate state where we're starting outside the field?
+    if start.loc.x.abs() >= rl::GOALPOST_X {
+        return None;
+    }
 
     let goal_y = rl::FIELD_MAX_Y * start.loc.y.signum();
     let ray = physics::car_forward_axis_2d(start.rot.to_2d());
