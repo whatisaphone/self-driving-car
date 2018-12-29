7a266e3f1f8e99766cbaf2181e079552ff15f55a
John Simon <john@whatisaph.one>
2018-12-29 05:24:45 -0500

    Add logging

--- brain/src/behavior/movement/get_to_flat_ground.rs
+++ brain/src/behavior/movement/get_to_flat_ground.rs
@@ -3,7 +3,7 @@ use crate::{
         higher_order::Chain,
         movement::{dodge::Dodge, drive_towards::drive_towards, yielder::Yielder},
     },
-    eeg::Drawable,
+    eeg::{color, Drawable},
     routing::{models::CarState, plan::avoid_goal_wall_waypoint},
     strategy::{Action, Behavior, Context, Priority},
 };
@@ -41,14 +41,18 @@ impl Behavior for GetToFlatGround {
 
         if me.OnGround && me.Physics.roof_axis().angle(&-Vector3::z_axis()) < PI / 10.0 {
             // We're probably upside down under the ceiling of a goal
+            ctx.eeg.log(self.name(), "jumping while upside-down");
             Action::Yield(rlbot::ffi::PlayerInput {
                 Jump: true,
                 ..Default::default()
             })
         } else if me.OnGround && me.Physics.forward_axis().angle(&Vector3::z_axis()) < PI / 4.0 {
             // Our nose is pointed towards the sky. It's quicker to jump down than to drive.
+            ctx.eeg
+                .draw(Drawable::print("nose pointed upwards", color::GREEN));
             if me.Physics.vel().z >= 0.0 || me.Physics.loc().z >= 1000.0 {
                 // Phase one of the reverse dismount: back up so we don't jump into the sky
+                ctx.eeg.draw(Drawable::print("backing up", color::GREEN));
                 Action::Yield(rlbot::ffi::PlayerInput {
                     Throttle: -1.0,
                     ..Default::default()
@@ -56,6 +60,7 @@ impl Behavior for GetToFlatGround {
             } else {
                 // Phase two of the reverse dismount: jump. The rotator below will make us land
                 // on our wheels.
+                ctx.eeg.log(self.name(), "jumping off the wall");
                 let mut inputs = Vec::<Box<Behavior>>::with_capacity(3);
                 inputs.push(Box::new(Yielder::new(
                     rlbot::ffi::PlayerInput {
@@ -69,6 +74,9 @@ impl Behavior for GetToFlatGround {
                 if defensiveness(ctx) < 0.5 {
                     // We're probably way out of the game. Dodge towards our goal to get back to
                     // defense quicker.
+                    ctx.eeg
+                        .log(self.name(), "... and dodging to get back quick");
+
                     let angle = (me.Physics.forward_axis().unwrap()
                         + me.Physics.roof_axis().unwrap())
                     .to_2d()
@@ -90,6 +98,8 @@ impl Behavior for GetToFlatGround {
                 Action::call(Chain::new(Priority::Idle, inputs))
             }
         } else if me.OnGround {
+            ctx.eeg
+                .draw(Drawable::print("driving down the wall", color::GREEN));
             let target_loc =
                 (me.Physics.loc() + me.Physics.rot() * Vector3::new(500.0, 0.0, 250.0)).to_2d();
             ctx.eeg
@@ -98,11 +108,15 @@ impl Behavior for GetToFlatGround {
         } else if me.Physics.ang_vel().norm() >= 5.0 {
             // This is a minor hack for statelessness. We're probably in the middle of a
             // dodge. Just sit tight.
+            ctx.eeg
+                .draw(Drawable::print("waiting until after flip", color::GREEN));
             Action::Yield(rlbot::ffi::PlayerInput {
                 Throttle: 1.0,
                 ..Default::default()
             })
         } else {
+            ctx.eeg.draw(Drawable::print("air rolling", color::GREEN));
+
             let facing = choose_facing(ctx);
 
             // Boost towards the ground if we're floating helplessly
@@ -116,8 +130,14 @@ impl Behavior for GetToFlatGround {
                 forward = facing.rotation_to(&-Vector3::z_axis()).powf(down_amount) * facing;
 
                 let nose_down_angle = me.Physics.forward_axis().angle_to(&-Vector3::z_axis());
+
+                ctx.eeg
+                    .print_value("down_amount", format!("{:.2}", down_amount));
+                ctx.eeg.print_angle("nose_down_angle", nose_down_angle);
+
                 boost = down_amount > 0.0 && nose_down_angle < PI / 3.0;
             } else {
+                ctx.eeg.draw(Drawable::print("no boost", color::GREEN));
                 forward = facing;
                 boost = false;
             }
@@ -135,23 +155,29 @@ impl Behavior for GetToFlatGround {
     }
 }
 
-#[allow(clippy::if_same_then_else)]
 fn choose_facing(ctx: &mut Context) -> Unit<Vector3<f32>> {
     let me = ctx.me();
 
     if me.Physics.loc().y.abs() >= ctx.game.field_max_y() {
         // If we're going to land in the goal, land in a convenient direction to
         // immediately drive out of the goal towards the ball.
+        ctx.eeg
+            .draw(Drawable::print("landing in a goal", color::GREEN));
         face_the_ball(ctx)
     } else if me.Physics.vel_2d().norm() < 500.0 {
         // If we're not moving much, we have no momentum to conserve, so face the ball.
+        ctx.eeg.draw(Drawable::print("no momentum", color::GREEN));
         face_the_ball(ctx)
     } else if defensiveness(ctx) >= 7.0 {
         // If we're playing defense, forget momentum, try to stay ready to challenge the
         // ball.
+        ctx.eeg
+            .draw(Drawable::print("defensive positioning", color::GREEN));
         face_the_ball(ctx)
     } else {
         // Conserve our momentum (i.e. don't skid on landing)
+        ctx.eeg
+            .draw(Drawable::print("conserving momentum", color::GREEN));
         me.Physics.vel_2d()
     }
     .to_3d(0.0)
